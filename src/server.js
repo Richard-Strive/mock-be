fefde0887a1ae5c6695c5e1ce2f37f3360b6a87c
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2022-03-18 00:06:49 +0100

    two

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const productsRouter = require("./routes");
 const server = express();
+const cors = require("cors");
+
+server.use(cors());
 
 server.use(express.json());
 
