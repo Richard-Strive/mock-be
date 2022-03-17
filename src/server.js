1bafd9f86fbf16f3133b0735961ad11d678ecb39
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2022-03-17 23:38:38 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 const productsRouter = require("./routes");
 const server = express();
 
-const port = 3001;
 server.use(express.json());
 
 server.use("/products", productsRouter);
 
-server.listen(port, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log("The server is successfully started");
 });
