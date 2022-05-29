2f42ffc7349d926961d44e86be134d7597a8dcf7
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2022-05-29 13:51:21 +0200

    Fix

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,26 +4,55 @@ function getHistoryPurchase() {
       type: "Attrazione",
       title: "Piazza San Marco",
       image: "/random.jpeg",
-      localtion:"Venezia, Veneto",
-      cane:"Lupo di mare di Venezia",
+      localtion:"Venezia",
     },
     {
       type: "hotel",
       title: "San Marco Palace",
       image: "/random.jpeg",
-      localtion:"Venezia, Veneto"
+      localtion:"Venezia"
     },
     {
       type: "restaurant",
       title: "Ristorante - $$$",
       image: "/random.jpeg",
-      localtion:"Venezia, Veneto"
+      localtion:"Venezia"
     },
     {
       type: "Visita con guida del posto in Piazza San Marco",
       title: "Attività - Durata 5 ore",
       image: "/random.jpeg",
-      localtion:"Venezia, Veneto"
+      localtion:"Venezia"
+    },
+    {
+      type: "Attrazione",
+      title: "Piazza San Marco",
+      image: "/random.jpeg",
+      localtion:"Milano",
+    },
+    {
+      type: "hotel",
+      title: "San Marco Palace",
+      image: "/random.jpeg",
+      localtion:"Milano"
+    },
+    {
+      type: "Visita con guida del posto in Piazza San Marco",
+      title: "Attività - Durata 5 ore",
+      image: "/random.jpeg",
+      localtion:"Napoli"
+    },
+    {
+      type: "hotel",
+      title: "San Marco Palace",
+      image: "/random.jpeg",
+      localtion:"Palermo"
+    },
+    {
+      type: "Visita con guida del posto in Piazza San Marco",
+      title: "Attività - Durata 5 ore",
+      image: "/random.jpeg",
+      localtion:"Palermo"
     },
 
   ];
